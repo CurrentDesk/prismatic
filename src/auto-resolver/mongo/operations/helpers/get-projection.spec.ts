48f10881deb5d05b27fb5b3ef4f0669c4745a78b
Patrick Reames <patrick.reames@gmail.com>
2018-04-16 18:10:52 -0500

    use x to skip a test

--- src/auto-resolver/mongo/operations/helpers/get-projection.spec.ts
+++ src/auto-resolver/mongo/operations/helpers/get-projection.spec.ts
@@ -1,7 +1,7 @@
-// import { getProjection } from './get-projection'
-//
-// describe('getProjection', () => {
-//   it('should return 1 for all projected fields', () => {
-//
-//   })
-// })
+import { getProjection } from './get-projection'
+
+describe('getProjection', () => {
+  xit('should return 1 for all projected fields', () => {
+
+  })
+})
