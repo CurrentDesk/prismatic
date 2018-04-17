fb664b4d25fa49ea3cbb3d808df3768d35292045
Patrick Reames <patrick.reames@gmail.com>
2018-04-17 15:49:05 -0500

    chore: tests for mapWhere and mapOrderBy

--- src/auto-resolver/mongo/operations/helpers/map-order-by.spec.ts
+++ src/auto-resolver/mongo/operations/helpers/map-order-by.spec.ts
@@ -4,12 +4,12 @@ describe('mapOrderBy', () => {
 
   it('should map ascending to 1', () => {
     const ascending = 'blah_ASC'
-    expect(mapOrderBy(ascending)).toMatchObject(['blah', 1])
+    expect(mapOrderBy(ascending)).toEqual(['blah', 1])
   })
 
   it('should map descending to -1', () => {
     const descending = 'blah_DESC'
-    expect(mapOrderBy(descending)).toMatchObject(['blah', -1])
+    expect(mapOrderBy(descending)).toEqual(['blah', -1])
   })
-  
+
 })
