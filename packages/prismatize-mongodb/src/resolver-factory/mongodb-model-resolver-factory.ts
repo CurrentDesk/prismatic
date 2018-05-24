feb88421fc81dcd4b9038d7de733f1dc6c95d81e
TJ Peden <tj.peden@tjcoding.com>
2018-05-24 16:35:45 -0500

    fix: embedded objects can still need lookups

--- packages/prismatize-mongodb/src/resolver-factory/mongodb-model-resolver-factory.ts
+++ packages/prismatize-mongodb/src/resolver-factory/mongodb-model-resolver-factory.ts
@@ -29,63 +29,64 @@ export class MongoDBModelResolverFactory extends ResolverFactory {
     }: ObjectTypeDefinitionNode
   ): Maybe<IResolverObject> {
     if (fields) {
-      if (!hasDirective(modelDirectives, 'embedded')) {
-        return fields.reduce((
-          resolvers: IResolverObject,
-          {
-            name: {
-              value: name,
-            },
-            type,
-            directives,
-          }
-        ) => {
-          const {
-            list,
-            namedType,
-          } = unwrap(type)
-          const gqlType = typeFromAST(this.schema, namedType)
+      return fields.reduce((
+        resolvers: IResolverObject,
+        {
+          name: {
+            value: name,
+          },
+          type,
+          directives,
+        }
+      ) => {
+        const {
+          list,
+          namedType,
+        } = unwrap(type)
+        const gqlType = typeFromAST(this.schema, namedType)
 
-          if (gqlType) {
-            const collection = tableize(namedType.name.value)
+        if (gqlType) {
+          const collection = tableize(namedType.name.value)
 
-            switch (name) {
-              case 'id': {
+          switch (name) {
+            case 'id': {
+              if (!hasDirective(modelDirectives, 'embedded')) {
                 if (!hasDirective(directives, 'unique')) {
                   throw new Error('Field `id` must be marked `@unique`.')
                 }
 
                 resolvers.id = (object) => object._id
-
-                break
               }
-              default: {
-                if (isObjectType(gqlType) && !hasDirective(directives, 'embedded')) {
-                  resolvers[name] = (source, args, context, info) => {
-                    if (list) {
-                      if (source[name] !== undefined) {
-                        Object.assign(args, { where: { id_in: source[name] } })
 
-                        return find(collection)(null, args, context, info)
-                      } else {
-                        return []
-                      }
+              break
+            }
+            default: {
+              if (isObjectType(gqlType) && !hasDirective(directives, 'embedded')) {
+                resolvers[name] = (source, args, context, info) => {
+                  console.log(name, source[name], source)
+                  if (source[name] !== undefined && source[name] !== null) {
+                    if (list) {
+                      Object.assign(args, { where: { id_in: source[name] } })
+
+                      return find(collection)(null, args, context, info)
                     }
 
                     Object.assign(args, { where: { id: source[name] } })
 
                     return findOne(collection)(null, args, context, info)
                   }
+
+                  return list ? [] : null
                 }
               }
             }
-          } else {
-            throw new Error('Field type not found in GraphQLSchema')
           }
+        } else {
+          throw new Error('Field type not found in GraphQLSchema')
+        }
 
-          return resolvers
-        }, {})
-      }
+        return resolvers
+      }, {})
     } else {
       throw new Error('Model ${modelName} has no fields')
     }
